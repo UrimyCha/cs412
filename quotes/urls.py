ea15df4b03a1a7f5bf4141d6b75de7fb8c331f8c
ucha <ucha@bu.edu>
2024-09-19 15:13:01 -0400

    began assignment3: quotes app

---
+++ quotes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+
+
+urlpatterns = [
+    path(r'', views.quote, name = "quote"),  #have to match views w/ home
+    path(r'about', views.about, name = "about")
+    path(r'show_all', view.show_all, name = "show_all")
+]
---
+++ quotes/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+import time
+import random
+
+# Create your views here.
+quotes = ["I don't like looking back. I'm always constantly looking forward. \
+        I'm not the one to sort of sit and cry over spilt milk. I'm too busy looking for the next cow.",
+        "That journey of coming back to the very top is better than actually being at the top. You find \
+        out so much about yourself and who your friends are.",
+        "Cooking is about passion, so it may look slightly temperamental in a way that it’s too assertive to the naked eye."
+        ]
+
+images = ["https://static.wikia.nocookie.net/hellskitchen/images/6/6f/Gordon_Ramsay.jpg/revision/latest?cb=20190118190638",
+          "https://hips.hearstapps.com/hmg-prod/images/host-judge-gordon-ramsay-in-the-hells-kitchen-semi-finals-news-photo-1718650477.jpg",
+          "https://media.timeout.com/images/105854659/image.jpg"]
+
+def quote(request):
+    # use this template to render the response
+    template_name = 'quotes/quote.html'
+
+    # create a dictionary of context variables for the template:
+    context = {
+        "current_time" : time.ctime(),
+        "quote" : random.choice(quotes),
+        "image" : random.choice(images),
+        "q1" : quotes[0],
+        "q2" : quotes[1],
+        "q3" : quotes[2],
+        "i1" : images[0],
+        "i2" : images[1],
+        "i3" : images[2]
+    }
+
+    # delegate rendering work to the template
+    return render(request, template_name, context)
