78e661e1c902a41ab073b50f0b60cd160fe3a34f
ucha <ucha@bu.edu>
2024-12-10 20:12:14 -0500

    Final Project trying to push to heroku

--- project/models.py
+++ project/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from datetime import datetime
 from django.contrib.auth.models import User
@@ -18,7 +17,7 @@ class Customer(models.Model):
     first_name = models.CharField(blank=False, max_length=100)
     last_name = models.CharField(blank=False, max_length=100)
     email = models.EmailField(blank=False)
-    phone_number = PhoneNumberField(blank=False, region="US")
+    phone_number = models.CharField(blank=False, max_length=11)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
